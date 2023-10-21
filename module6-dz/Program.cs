d5878cab54465ed2a07d9bbd1fe8947e4261996c
Nurkhadis <nurkhadissandibek@gmail.com>
2023-10-21 22:22:49 +0600

    Добавьте файлы проекта.

---
+++ module6-dz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MyLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Age = 30
+        };
+
+        string personInfo = PersonUtility.GetPersonInfo(person);
+        Console.WriteLine(personInfo);
+    }
+}
